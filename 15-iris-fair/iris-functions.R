551cd716a87da9fe6f7ab4b2aa2b3d0d9a0a64de
btmonier <brandon.monier@gmail.com>
2018-03-30 14:35:37 -0500

    Add scalable text box function

--- 15-iris-fair/iris-functions.R
+++ 15-iris-fair/iris-functions.R
@@ -225,6 +225,28 @@ getContTable <- function(
   return(de.genes2)
 }
 
+# Big text box
+textAreaInput2 <- function (
+  inputId, label, value = "", width = NULL, height = NULL, 
+  cols = NULL, rows = NULL, placeholder = NULL, resize = NULL
+) {
+    value <- restoreInput(id = inputId, default = value)
+    if (!is.null(resize)) {
+        resize <- match.arg(resize, c("both", "none", "vertical", 
+            "horizontal"))
+    }
+    style <- paste("max-width: 100%;", if (!is.null(width)) 
+        paste0("width: ", validateCssUnit(width), ";"), if (!is.null(height)) 
+        paste0("height: ", validateCssUnit(height), ";"), if (!is.null(resize)) 
+        paste0("resize: ", resize, ";"))
+    if (length(style) == 0) 
+        style <- NULL
+    div(class = "form-group", 
+        tags$label(label, `for` = inputId), tags$textarea(id = inputId, 
+        class = "form-control", placeholder = placeholder, style = style, 
+        rows = rows, cols = cols, value))
+}
+
 
 
 # LIMMA RETURN FIT functions
